e4f04bd5ca993cad12b8e439462b5a03514b7b78
Digambar <deshawaldigambar@gmail.com>
2022-04-03 21:16:40 +0530

    feat : added products in Database

--- src/backend/db/products.js
+++ src/backend/db/products.js
@@ -8,23 +8,240 @@ import { v4 as uuid } from "uuid";
 export const products = [
   {
     _id: uuid(),
-    title: "You Can WIN",
-    author: "Shiv Khera",
-    price: "5000",
-    categoryName: "non-fiction",
+    name:"Lenove IdeaPad 3",
+    brand:"lenovo",
+    price: "50000",
+    processor:"core i5",
+    ram:"8 GB",
+    rating:"5",
+    details:
+      `512 GB SSD/Windows 10 Home15IML05
+      Thin and Light Laptop  (
+      15.6 Inch, Platinum Grey, 1.7 kg, 
+      With MS Office)`
   },
   {
     _id: uuid(),
-    title: "You are Winner",
-    author: "Junaid Qureshi",
-    price: "3000",
-    categoryName: "horror",
+    name:"Lenove IdeaPad 3",
+    brand:"lenovo",
+    price: "35000",
+    processor:"core i3",
+    ram:"8 GB",
+    rating:"4",
+    details:
+      `512 GB SSD/Windows 10 Home15IML05
+      Thin and Light Laptop  (
+      15.6 Inch, Platinum Grey, 1.7 kg, 
+      With MS Office)`
   },
   {
     _id: uuid(),
-    title: "Think and Grow Rich",
-    author: "Shiv Khera",
-    price: "1000",
-    categoryName: "fiction",
+    name:"Lenove ThinkPad E15",
+    brand:"lenovo",
+    price: "44,950",
+    processor:"core i3",
+    ram:"4 GB",
+    rating:"4",
+    details:
+      `256 GB SSD/Windows 10 Home15IML05
+      Thin and Light Laptop  (
+      15.6 Inch, Black, 1.6 kg, 
+      With MS Office)`
   },
+  {
+    _id: uuid(),
+    name:"Lenove V15",
+    brand:"lenovo",
+    price: "50000",
+    processor:"core i5",
+    ram:"8 GB",
+    rating:"3",
+    details:
+      `512 GB SSD/Windows 10 Home15IML05
+      Thin and Light Laptop  (
+      15.6 Inch, Platinum Grey, 1.7 kg, 
+      With MS Office)`
+  },
+  {
+    _id: uuid(),
+    name:"Lenove IdeaPad Slim",
+    brand:"lenovo",
+    price: "39999",
+    processor:"core i3",
+    ram:"4 GB",
+    rating:"3",
+    details:
+      `256 GB SSD/Windows 10 Home15IML05
+      Thin and Light Laptop  (
+      15.6 Inch, Platinum Grey, 1.9 kg, 
+      With MS Office)`
+  },
+  {
+    _id: uuid(),
+    name:"Dell G15",
+    brand:"dell",
+    price: "79990",
+    processor:"core i5",
+    ram:"16 GB",
+    rating:"5",
+    details:
+      `512 GB SSD/Windows 10 Home15IML05
+      Thin and Light Laptop  (
+      14.96 Inch, Platinum Grey, 1.65 kg, 
+      With MS Office)`
+  },
+  {
+    _id: uuid(),
+    name:"Dell inpiron 7501",
+    brand:"dell",
+    price: "79990",
+    processor:"core i5",
+    ram:"8 GB",
+    rating:"3",
+    details:
+      `512 GB SSD/Windows 10 Home15IML05
+      Thin and Light Laptop, 4GB Graphics (
+      14.96 Inch, Platinum Grey, 1.7 kg, 
+      With MS Office)`
+  },
+  {
+    _id: uuid(),
+    name:"Dell XPS",
+    brand:"dell",
+    price: "1,11,890",
+    processor:"core i5",
+    ram:"8 GB",
+    rating:"4",
+    details:
+      `512 GB SSD/Windows 10 Home15IML05
+      Thin and Light Laptop  (
+      15.6 Inch, Grey, 1.4 kg, 
+      With MS Office)`
+  },
+  {
+    _id: uuid(),
+    name:"Dell Vostro ",
+    brand:"dell",
+    price:"38990",
+    processor:"core i3",
+    ram:"8 GB",
+    rating:"4",
+    details:
+      `256 GB SSD/Windows 10 Home15IML05
+      Thin and Light Laptop  (
+      14.96 Inch, Platinum Grey, 1.7 kg, 
+      With MS Office)`
+  },
+  {
+    _id: uuid(),
+    name:"Dell Inspiron",
+    brand:"dell",
+    price: "42490",
+    processor:"core i3",
+    ram:"8 GB",
+    rating:"5",
+    details:
+      `1 TB HDD/Windows 10 Home15IML05
+      Thin and Light Laptop  (
+      15.6 Inch, Platinum Grey, 2.1 kg, 
+      With MS Office)`
+  },
+  {
+    _id: uuid(),
+    name:"Asus Vivobook 14",
+    brand:"Asus",
+    price: "31980",
+    processor:"core i3",
+    ram:"8 GB",
+    rating:"4",
+    details:
+      `1 TB SSD/Windows 10 Home15IML05
+      Thin and Light Laptop  (
+      14.1 Inch, Platinum Grey, 1.7 kg, 
+      With MS Office)`
+  },
+  {
+    _id: uuid(),
+    name:"Asus Vivobook 15",
+    brand:"asus",
+    price: "50000",
+    processor:"core i3",
+    ram:"4 GB",
+    rating:"3",
+    details:
+      `1 TB SSD/Windows 10 Home15IML05
+      Thin and Light Laptop  (
+      15.6 Inch, Platinum Grey, 1.7 kg, 
+      With MS Office)`
+  },
+  {
+    _id: uuid(),
+    name:"Asus Vivobook Ultra",
+    brand:"Asus",
+    price: "40990",
+    processor:"core i3",
+    ram:"4 GB",
+    rating:"4",
+    details:
+      `512 GB SSD/Windows 10 Home15IML05
+      Thin and Light Laptop  (
+      15.6 Inch, Platinum Grey, 1.7 kg, 
+      With MS Office)`
+  },
+  {
+    _id: uuid(),
+    name:"Asus TUF",
+    brand:"asus",
+    price: "64990",
+    processor:"core i5",
+    ram:"8 GB",
+    rating:"4",
+    details:
+      `512 GB SSD/Windows 10 Home15IML05
+      Thin and Light Laptop  (
+      15.6 Inch, Platinum Grey, 1.7 kg, 
+      With MS Office)`
+  },
+  {
+  _id: uuid(),
+    name:"HP Pavilion",
+    brand:"hp",
+    price: "53990",
+    processor:"core i3",
+    ram:"8 GB",
+    rating:"3",
+    details:
+      `256 GB SSD/Windows 10 Home15IML05
+      Thin and Light Laptop  (
+      15.6 Inch, Platinum Grey, 1.7 kg, 
+      With MS Office)`
+  },
+  {
+    _id: uuid(),
+      name:"HP NoteBook",
+      brand:"hp",
+      price: "40990",
+      processor:"core i3",
+      ram:"8 GB",
+      rating:"4",
+      details:
+        `1 TB SSD/Windows 10 Home15IML05
+        Thin and Light Laptop  (
+        14 Inch, Platinum Grey, 1.7 kg, 
+        With MS Office)`
+    },
+    {
+      _id: uuid(),
+        name:"HP Corer i5",
+        brand:"hp",
+        price: "42997",
+        processor:"core i5",
+        ram:"8 GB",
+        rating:"5",
+        details:
+          `512 GB SSD/Windows 10 Home15IML05
+          Thin and Light Laptop  (
+          14 Inch, Platinum Grey, 165 kg, 
+          With MS Office)`
+      }
 ];
