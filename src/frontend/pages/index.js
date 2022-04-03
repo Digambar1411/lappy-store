bfb3daeb1a7e007721fa7805ff8fda9002b598b5
Digambar <deshawaldigambar@gmail.com>
2022-04-03 23:50:34 +0530

    feat: added react router

---
+++ src/frontend/pages/index.js
@@ -0,0 +1,6 @@
+export { Products } from "./products/products";
+export { Login } from "./login/login";
+export { Cart } from "./cart/cart.jsx";
+export { Wishlist } from  "./wishlist/wishlist";
+export { Signup } from "./signup/signup";
+export { Home } from "./home/home";
---
+++ src/frontend/pages/wishlist/wishlist.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+export function Wishlist(){
+    return(
+        <h1> wishlist page </h1>
+    );
+}
