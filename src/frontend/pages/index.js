203b7b68db5acd35bc3f805cb461a7988af65852
Digambar <deshawaldigambar@gmail.com>
2022-04-04 20:48:02 +0530

    fix: cart page error

--- src/frontend/pages/index.js
+++ src/frontend/pages/index.js
@@ -1,6 +1,6 @@
 export { Products } from "./products/products";
 export { Login } from "./login/login";
-export { Cart } from "./cart/cart.jsx";
+export { Cart } from "./cart/cart";
 export { Wishlist } from  "./wishlist/wishlist";
 export { Signup } from "./signup/signup";
 export { Home } from "./home/home";
