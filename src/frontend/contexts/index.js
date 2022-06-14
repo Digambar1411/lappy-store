cf4fb2e7485db1ce27f7a20e2d210bb0578cb8e8
Digambar <deshawaldigambar@gmail.com>
2022-06-14 17:18:16 +0530

    feat : barrel import/export

---
+++ src/frontend/contexts/index.js
@@ -0,0 +1,13 @@
+import {CategoryProvider} from "./category-context";
+import { CartProvider } from "./cart-context";
+import { WishlistProvider } from "./wishlist-context";
+import { FilterProvider } from "./filter-context";
+import {ProductProvider} from "./product-context";
+
+export {
+    CartProvider,
+    WishlistProvider,
+    FilterProvider,
+    ProductProvider,
+    CategoryProvider
+}
--- src/index.js
+++ src/index.js
@@ -4,22 +4,32 @@ import "./index.css";
 import App from "./App";
 import { makeServer } from "./server";
 import { BrowserRouter } from "react-router-dom";
-import { FilterProvider } from "./frontend/contexts/filter-context";
-import { CartProvider } from "./frontend/contexts/cart-context";
-import { WishlistProvider } from "./frontend/contexts/wishlist-context";
+import { 
+  FilterProvider, 
+  CartProvider, 
+  WishlistProvider, 
+  ProductProvider,
+  CategoryProvider
+} from "./frontend/contexts/index.js"
+
+
 
 makeServer();
 
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
-      <FilterProvider>
-        <CartProvider>
-          <WishlistProvider>
-            <App/>
-          </WishlistProvider>
-        </CartProvider>
-      </FilterProvider>
+      <CategoryProvider>
+        <ProductProvider>
+          <FilterProvider>
+            <CartProvider>
+              <WishlistProvider>
+                <App/>
+              </WishlistProvider>
+            </CartProvider>
+          </FilterProvider>
+        </ProductProvider>
+      </CategoryProvider>
     </BrowserRouter>
   </React.StrictMode>,
   document.getElementById("root")
