2c7a6f9bb3f19f031b202540ad10dd928095606d
Digambar <deshawaldigambar@gmail.com>
2022-10-27 20:41:42 +0530

    added category images

--- src/backend/db/categories.js
+++ src/backend/db/categories.js
@@ -10,18 +10,21 @@ export const categories = [
   {
     _id: uuid(),
     categoryName: "Gaming Laptop",
+    image:["product-assets/lenovo-ideapad-3-gaming.jpg"],
     description:
       "Powerful gaming setups for brilliant wins.",
   },
   {
     _id: uuid(),
     categoryName: "Budget Laptop",
+    image:["product-assets/asus-vivobook-15.jpg"],
     description:
       "Learning Made Engaging and Fun. Exclusive Deals with Great Offers.",
   },
   {
     _id: uuid(),
     categoryName: "Thin and Light Laptop",
+    image:["product-assets/dell_g15.avif"],
     description:
       "Reimagine the Way Your Kids Learn No Cost EMI & more",
   }
