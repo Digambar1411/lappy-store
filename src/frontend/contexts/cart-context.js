6f0ee8a1007dbe1014795ffecd780db930385049
Digambar <deshawaldigambar@gmail.com>
2022-10-29 13:16:15 +0530

    fix: decrement item in cart

--- src/frontend/contexts/cart-context.js
+++ src/frontend/contexts/cart-context.js
@@ -1,5 +1,5 @@
-import {createContext, useContext,useState} from "react";
-import { useAuth} from "./auth-context"
+import { createContext, useContext, useState } from "react";
+import { useAuth } from "./auth-context";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
 
@@ -7,30 +7,34 @@ const CartContext = createContext();
 
 const useCart = () => useContext(CartContext);
 
-const CartProvider = ({children}) => {
-  const { authState:{token, isLoggedIn}}= useAuth();
-  const [cart, setCart] = useState([]);
-  const navigate = useNavigate();
-  
+const CartProvider = ({ children }) => {
+	const {
+		authState: { token, isLoggedIn },
+	} = useAuth();
+	const [cart, setCart] = useState([]);
+	const navigate = useNavigate();
+
 	const addToCart = async (product) => {
-    if(isLoggedIn){
-      try {
-        const response = await axios.post("/api/user/cart", 
-        {
-          product
-        },
-        {
-          headers: {
-              authorization: token,
-                  },
-        });
-        setCart(response.data.cart)
-      } catch (error) {
-        console.error(error);
-      }
-    }else{
-      navigate("/login")
-    }
+		if (isLoggedIn) {
+			try {
+				const response = await axios.post(
+					"/api/user/cart",
+					{
+						product,
+					},
+					{
+						headers: {
+							authorization: token,
+						},
+					}
+				);
+				setCart(response.data.cart);
+			} catch (error) {
+				console.error(error);
+			}
+		} else {
+			navigate("/login");
+		}
 	};
 
 	const removeFromCart = async (productId) => {
@@ -40,33 +44,52 @@ const CartProvider = ({children}) => {
 					authorization: token,
 				},
 			});
-      setCart(response.data.cart)
+			setCart(response.data.cart);
 		} catch (error) {
 			console.error(error);
 		}
 	};
 
-  const incrementQuantity=(product)=>{
-    setCart(cart.map(item=>item._id===product._id ? {...item,quantity:item.quantity+1} :item))
-   
-  }
+	const incrementQuantity = (product) => {
+		setCart(
+			cart.map((item) =>
+				item._id === product._id
+					? { ...item, quantity: item.quantity + 1 }
+					: item
+			)
+		);
+	};
 
-  const decrementQuantity=(product)=>{
-    setCart(cart.map(item=>item._id===product._id ? {...item,quantity:(item.quantity ? 1 : 0)} :item))
+	const decrementQuantity = (product) => {
+		setCart(
+			cart.map((item) =>
+				item._id === product._id
+					? { ...item, quantity: item.quantity>1 ? item.quantity - 1 : 1 }
+					: item
+			)
+		);
+	};
 
-  }
+	const moveToWishlist = (id) => {
+		const newData1 = cart.filter((item) => item._id !== id);
+		setCart(newData1);
+	};
 
-    const moveToWishlist = (id) =>{
-        const newData1=cart.filter(item=>item._id !== id)
-        setCart(newData1);
-    }
+	return (
+		<CartContext.Provider
+			value={{
+				cart,
+				setCart,
+				addToCart,
+				removeFromCart,
+				moveToWishlist,
+				incrementQuantity,
+				decrementQuantity,
+			}}
+		>
+			{children}
+		</CartContext.Provider>
+	);
+};
 
-    return ( 
-        <CartContext.Provider value={{ cart, setCart, addToCart, removeFromCart, moveToWishlist, incrementQuantity, decrementQuantity}}>
-            {children}
-        </CartContext.Provider>
-
-    )
-}
-
-export { useCart, CartProvider}
+export { useCart, CartProvider };
